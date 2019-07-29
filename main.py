555c48d2cbd88bba973c1361d22020393725d32f
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-29 09:51:28 -0700

    Renamed login to main, added user API

---
+++ main.py
@@ -0,0 +1,60 @@
+import webapp2
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+class CssiUser(ndb.Model):
+  first_name = ndb.StringProperty()
+  last_name = ndb.StringProperty()
+  email = ndb.StringProperty()
+
+class MainHandler(webapp2.RequestHandler):
+  def get(self):
+    user = users.get_current_user()
+    # If the user is logged in...
+    if user:
+      signout_link_html = '<a href="%s">sign out</a>' % (
+          users.create_logout_url('/'))
+      email_address = user.nickname()
+      cssi_user = CssiUser.query().filter(CssiUser.email == email_address).get()
+      # If the user is registered...
+      if cssi_user:
+        # Greet them with their personal information
+        self.response.write('''
+            Welcome %s %s (%s)! <br> %s <br>''' % (
+              cssi_user.first_name,
+              cssi_user.last_name,
+              email_address,
+              signout_link_html))
+      # If the user isn't registered...
+      else:
+        # Offer a registration form for a first-time visitor:
+        self.response.write('''
+            Welcome to our site, %s!  Please sign up! <br>
+            <form method="post" action="/">
+            <input type="text" name="first_name">
+            <input type="text" name="last_name">
+            <input type="submit">
+            </form><br> %s <br>
+            ''' % (email_address, signout_link_html))
+    else:
+      # If the user isn't logged in...
+      login_url = users.create_login_url('/')
+      login_html_element = '<a href="%s">Sign in</a>' % login_url
+      # Prompt the user to sign in.
+      self.response.write('Please log in.<br>' + login_html_element)
+
+  def post(self):
+    # Code to handle a first-time registration from the form:
+    user = users.get_current_user()
+    cssi_user = CssiUser(
+        first_name=self.request.get('first_name'),
+        last_name=self.request.get('last_name'),
+        email=user.nickname())
+    cssi_user.put()
+    self.response.write('Thanks for signing up, %s! <br><a href="/">Home</a>' %
+        cssi_user.first_name)
+
+app = webapp2.WSGIApplication([
+  ('/', MainHandler)
+], debug=True)
