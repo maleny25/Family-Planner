fb24b87f7acbc91b41abc7723cdecf9d381d4be9
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-30 11:46:44 -0700

    Fixed data storage issues, finished html for profile, changed class name of CSSiUser to User and all other code in main.py

--- login.py
+++ login.py
@@ -3,7 +3,10 @@ import webapp2
 # from google.appengine.api import users
 from google.appengine.ext import ndb
 
-class CssiUser(ndb.Model):
+class User(ndb.Model):
   first_name = ndb.StringProperty()
   last_name = ndb.StringProperty()
   email = ndb.StringProperty()
+
+class Family (ndb.Model):
+    members= ndb.KeyProperty(User, repeated=True)
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import webapp2
 import jinja2
 import os
 from google.appengine.api import users
-from login import CssiUser
+from login import User
+from login import Family
 import urllib
 
 the_jinja_env= jinja2.Environment(
@@ -10,18 +11,23 @@ the_jinja_env= jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+def load_family_by_email (email):
+    current_user=User.query().filter(User.email==email).fetch()[0]
+    family = Family.query(Family.members ==current_user.key).fetch()[0]
+    return family
+
 
 class MainHandler(webapp2.RequestHandler):
   def get(self):
-    user = users.get_current_user()
-    if user:
+    current_user = users.get_current_user()
+    if current_user:
       signout_link_html = '<a href="%s">sign out</a>' % (users.create_logout_url('/'))
-      email_address = user.email()
-      cssi_user = CssiUser.query().filter(CssiUser.email == email_address).get()
-      if cssi_user:
+      email_address = current_user.email()
+      user = User.query().filter(User.email == email_address).get()
+      if user:
         self.response.write(signout_link_html)
         calendar_template= the_jinja_env.get_template('templates/calendar.html')
-        first_name=cssi_user.first_name
+        first_name=user.first_name
         calendar_dict={
         "first_name":first_name
         }
@@ -32,7 +38,6 @@ class MainHandler(webapp2.RequestHandler):
             <form method="post" action="/">
             First Name: <input type="text" name="first_name"> <br>
             Last Name: <input type="text" name="last_name"> <br>
-            Email:<input type="text name="email" <br>
             <input type="submit">
             </form><br> %s <br>
             ''' % (email_address, signout_link_html))
@@ -43,15 +48,20 @@ class MainHandler(webapp2.RequestHandler):
 
 
   def post(self):
-    user = users.get_current_user()
-    cssi_user = CssiUser(
+    current_user = users.get_current_user()
+    user = User(
         first_name=self.request.get('first_name'),
         last_name=self.request.get('last_name'),
-        email=user.email())
-    cssi_user.put()
+        email=current_user.email())
+    user_key=user.put()
+    family=Family(
+        members=[user_key]
+    )
+    family.put()
     profile_template= the_jinja_env.get_template('templates/profile.html')
     profile_dict={
-    "last_name": cssi_user.last_name
+    "last_name": user.last_name,
+    "family":family
     }
     self.response.write(profile_template.render(profile_dict))
 
@@ -66,24 +76,36 @@ class Calendar(webapp2.RequestHandler):
 
 class Profile(webapp2.RequestHandler):
     def get(self):
-        user1 = users.get_current_user().nickname()
-        user = CssiUser.query().filter(CssiUser.email== user1).get()
-        last_name=user.get_current_user().last_name()
+        user1 = users.get_current_user().email()
+        user = User.query().filter(User.email== user1).get()
+        last_name=user.last_name
+        family= load_family_by_email(users.get_current_user().email())
         profile_template= the_jinja_env.get_template('templates/profile.html')
         profile_dict={
+        "family":family,
         "last_name": last_name,
         }
         self.response.write(profile_template.render(profile_dict))
 
     def post(self):
-        cssi_user = CssiUser(
+        user = User(
             first_name=self.request.get('Firstname'),
             last_name=self.request.get('Lastname'),
             email=self.request.get('email'))
-        cssi_user.put()
+
+        member=user.put()
+        family= load_family_by_email(users.get_current_user().email())
+        #adding user to family FamilyMembers
+        family.members.append(member)
+        #put the Family
+        family.put()
+
         profile_template= the_jinja_env.get_template('templates/profile.html')
         profile_dict={
-        "last_name": cssi_user.last_name
+        "last_name": user.last_name,
+        "first_name":user.first_name,
+        "email":user.email,
+        "family":family,
         }
         self.response.write(profile_template.render(profile_dict))
 
