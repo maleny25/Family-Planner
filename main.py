5f4cddea593a8fef1b8661f95a3b47950f3a7a8f
cssi <cssi@cssis-MacBook-Pro.local>
2019-08-01 15:18:16 -0700

    fixed lavendar to lavender

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def load_event (email):
 
 class MainHandler(webapp2.RequestHandler):
   def get(self):
-    colors=["Blue", "Brown", "Cyan", "Gold", "Gray", "Green", "Lavendar", "Lime", "Magenta", "Navy", "Orange", "Pink", "Purple","Turquoise", "Red", "Yellow"]
+    colors=["Blue", "Brown", "Cyan", "Gold", "Gray", "Green", "Lavender", "Lime", "Magenta", "Navy", "Orange", "Pink", "Purple","Turquoise", "Red", "Yellow"]
     current_user = users.get_current_user()
     if current_user:
       signout_link_html = '<a href="%s">sign out</a>' % (users.create_logout_url('/'))
@@ -45,7 +45,7 @@ class MainHandler(webapp2.RequestHandler):
       if user:
         self.redirect("/calendar")
       else:
-        colors=["Blue", "Brown", "Cyan", "Gold", "Gray", "Green", "Lavendar", "Lime", "Magenta", "Navy", "Orange", "Pink", "Purple","Turquoise", "Red", "Yellow"]
+        colors=["Blue", "Brown", "Cyan", "Gold", "Gray", "Green", "Lavender", "Lime", "Magenta", "Navy", "Orange", "Pink", "Purple","Turquoise", "Red", "Yellow"]
         current_user = users.get_current_user()
         # user_color=""
         # for color in colors:
@@ -127,7 +127,7 @@ class Calendar(webapp2.RequestHandler):
             event_date= event_date,
             event_end=event_end,
             color=color,
-            all_members= all_members,
+            # all_members= all_members,
         )
         calevent=event.put()
         time.sleep(0.1)
@@ -145,7 +145,7 @@ class Calendar(webapp2.RequestHandler):
 
 class Profile(webapp2.RequestHandler):
     def get(self):
-        colors=["Blue", "Brown", "Cyan", "Gold", "Gray", "Green", "Lavendar", "Lime", "Magenta", "Navy", "Orange", "Pink", "Purple","Turquoise", "Red", "Yellow"]
+        colors=["Blue", "Brown", "Cyan", "Gold", "Gray", "Green", "Lavender", "Lime", "Magenta", "Navy", "Orange", "Pink", "Purple","Turquoise", "Red", "Yellow"]
         current_user = users.get_current_user()
         user1 = users.get_current_user().email()
         user = User.query().filter(User.email== user1).get()
